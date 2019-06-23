06fe0ab0ea03e352f81b9768ac91620cc10fb8d1
Nicolas Béguier <nicolas_beguier@hotmail.com>
2019-06-23 21:08:42 +0200

    Factorize gen_action.sh, remove duplicates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # privoxy
+
+```
+bash gen_action.sh https://easylist.to/easylist/easylist.txt easylist
+
+bash gen_action.sh https://easylist.to/easylist/easyprivacy.txt easylistprivacy
+```
+
---
+++ gen_action.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+URL=$1
+NAME=$2
+TMPFILE="/tmp/${NAME}.txt"
+
+if ! grep -q "^actionsfile ${NAME}.action" /etc/privoxy/config; then
+    echo "[WARNING] Add 'actionsfile ${NAME}.action' in /etc/privoxy/config file"
+fi
+
+rm -f "${TMPFILE}"
+wget "${URL}" -O "${TMPFILE}" 
+
+echo "{ +block{${NAME}} }" > "/etc/privoxy/${NAME}.action"
+
+grep '\^\$third-party' "${TMPFILE}" | 
+    tr '@@||' '\n.' |
+    sed 's/\^\$third-party//g' |
+    cut -c 2- |
+    sed -E 's/\.([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/\1/g' |
+    sort -u |
+    grep -E '^[a-zA-Z0-9.-]+$' >> "/etc/privoxy/${NAME}.action"
+
